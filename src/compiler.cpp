9ac4c9a70c2594d473f51cb68e3d450839f71726
ayush <ayushpareek1980@gmail.com>
2025-08-11 02:30:02 +0530

    flow done

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,41 +1,85 @@
 #include "compiler.h"
 
+
 cyon::compiler::compiler(
-    const std::string& compiled_dir,
-    const std::unordered_map<std::string, std::vector<std::string>>& dependencies,
-    const std::unordered_map<std::string, std::string>& path_to_header,
-    const std::unordered_map<std::string, std::string>& path_to_source
+     std::string& compiled_dir,
+     std::unordered_map<std::string, std::vector<std::string>>& dependencies,
+     std::unordered_map<std::string, std::string>& path_to_header,
+     std::unordered_map<std::string, std::string>& path_to_source
 )
     : compiled_dir(compiled_dir),
       dependencies(dependencies),
       path_to_header(path_to_header),
       path_to_source(path_to_source)
-{}
+{
+    create_dir();
+    
+}
 
 void cyon::compiler::execute(){
-    // for(auto itr=dependencies.rbegin();itr!=dependencies.rend();itr++){
 
-    //     for(std::string _dep:itr->second){
-    //         if(dependencies.count(_dep)==0 || dependencies[_dep].size()==0){
-    //             compile(_dep);
+    for(auto itr=dependencies.begin();itr!=dependencies.end();itr++){
+        compile(itr->first,itr->second);
+    }
 
-    //         }
-    //         else if(is_compiling.count(_dep)>0 || is_compiled.count(_dep)>0){
-    //             continue;
-    //         }
-    //         else{
-    //             is_compiling.insert(_dep);
-    //         }
-    //     }
+    linker _linker(compiled_dir,object_files);
+    
+    
+}
 
-    // }
+void cyon::compiler::create_dir(){
+    if(!fs::exists(compiled_dir)){
+        if(fs::create_directory(compiled_dir)){
+            std::cout<<"temp folder for object files created at "<<std::endl;
+        }
+        else{
+            std::cerr<<"Failed to create"<<std::endl;
+        }
+    }
+    else{
+         std::cout<<"Already Exists"<<std::endl;
+    }
+}
+
+std::string cyon::compiler::trim_path(std::string path_to_file){
+        if (path_to_file.size() > 2 && (path_to_file.compare(path_to_file.size()-2, 2, ".h") == 0 ||
+                                path_to_file.compare(path_to_file.size()-4, 4, ".cpp") == 0)){
+            path_to_file.erase(path_to_file.find_last_of('/'));
+            path_to_file.erase(path_to_file.find_last_not_of(" \n\r\t") + 1);
+            return path_to_file;
+        }
+        std::cout<<"Problem with the path"<<std::endl;
+        return "";     
+} 
+
+std::string cyon::compiler::trim_name(std::string name){
+        if (name.size() > 2 && (name.compare(name.size()-2, 2, ".h") == 0 ||
+                                name.compare(name.size()-4, 4, ".cpp") == 0)){
+            name.erase(name.find_last_of('.'));
+            name.erase(name.find_last_not_of(" \n\r\t") + 1);
+            return name;
+        }
+        std::cout<<"Problem with the file name"<<std::endl;
+        return ""; 
+
 }
 
 
-void cyon::compiler::compile(std::string _file_name){
-    std::string _cli= base_command + path_to_source[_file_name]+ " -I"+path_to_header[_file_name] +" -o "+ compiled_dir+"/"+_file_name;
+void cyon::compiler::compile(std::string _file_name,std::vector<std::string> _dependencies){
+    std::string name=trim_name(_file_name);
+    
+    std::string header_inc="";
+    for(auto _dep:_dependencies){
+        _dep.erase(_dep.find_last_not_of(" \n\r\t") + 1);
+        header_inc+= "-I"+trim_path(path_to_header[_dep])+" ";
+    }
 
-    std::cout<<_cli;
 
-    // FILE* pip
+    std::string _cli= base_command + path_to_source[name+".cpp"]+ " "+header_inc+" -o "+ compiled_dir+"/"+name+".o";
+    object_files.push_back(name+".o");
+    run_command(_cli);
+}
+
+void cyon::compiler::run_command(std::string command){
+    FILE* pipe=popen(command.c_str(),"r");
 }
--- src/service.cpp
+++ src/service.cpp
@@ -2,20 +2,6 @@
 #include "service.h"
 
 
-void cyon::scan_deps::create_temp_dir(){
-    if(!fs::exists(compiled_dir)){
-        if(fs::create_directory(compiled_dir)){
-            std::cout<<"temp folder for object files created at "<<std::endl;
-        }
-        else{
-            std::cerr<<"Failed to create"<<std::endl;
-        }
-    }
-    else{
-         std::cout<<"Already Exists";
-    }
-}
-
 void cyon::scan_deps::fill_paths(std::string _filename){
     _filename.erase(_filename.find_last_not_of(" \n\r\t") + 1);
     // std::cout<<"searching for "<<_filename<<" "<<_filename.substr(_filename.length()-3)<<std::endl;
@@ -61,7 +47,6 @@ void cyon::scan_deps::run_service(){
             if(is_processed.count(_new_dep)>0 || in_process.count(_new_dep)>0){
                 continue;
             }
-            std::cout<<"going for scan"<<std::endl;
             fill_paths(_new_dep);
             _pending.push(_new_dep);
             dependencies[_file_name].push_back(_new_dep);
@@ -69,6 +54,9 @@ void cyon::scan_deps::run_service(){
         is_processed.insert(_file_name);
         in_process.erase(_file_name);
     }
+
+    compiler _compiler(compiled_dir,cyon::scan_deps::dependencies,cyon::scan_deps::path_to_header,cyon::scan_deps::path_to_source);
+    _compiler.execute();
 }
 
 void cyon::scan_deps::print_paths(){
